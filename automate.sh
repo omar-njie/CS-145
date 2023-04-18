1f7697aca9620de07507c67eb1b20e79ab7c1b7f
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2023-04-18 01:23:40 -0700

    Added Lectures 04-18-2023 ch 9

--- automate.sh
+++ automate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # shellcheck disable=SC2164
-cd /Users/omar/Desktop/College/Computer Science/Spring-Quarter-2023/CS-145
+cd /Users/omar/Desktop/College/Computer\ Science/Spring-Quarter-2023/CS-145
 
 git add .
 
