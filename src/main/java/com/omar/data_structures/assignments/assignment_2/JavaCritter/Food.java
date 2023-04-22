055b2b59c015bd8a530b4564297ad14d0a5f9b26
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2023-04-21 17:45:38 -0700

    Update

---
+++ src/main/java/com/omar/data_structures/assignments/assignment_2/JavaCritter/Food.java
@@ -0,0 +1,18 @@
+package com.omar.data_structures.assignments.assignment_2.JavaCritter;// This defines a simple class of critters that sit around waiting to be
+// taken over by other critters.
+
+import java.awt.*;
+
+public class Food extends Critter {
+    public Critter.Action getMove(CritterInfo info) {
+        return Critter.Action.INFECT;
+    }
+
+    public Color getColor() {
+        return Color.GREEN;
+    }
+
+    public String toString() {
+        return "F";
+    }
+}
