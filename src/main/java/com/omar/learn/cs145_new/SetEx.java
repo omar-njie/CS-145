e882753eadabc0f835bb2bd7fdcd0246a6deb1be
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2023-04-26 00:27:24 -0700

    Update

--- src/main/java/com/omar/learn/cs145_new/SetEx.java
+++ src/main/java/com/omar/learn/cs145_new/SetEx.java
@@ -1,8 +1,24 @@
 package com.omar.learn.cs145_new;
 
 /**
- * @author omar
+ * @author Omar
  * @version 4/26/23
  */
 public class SetEx {
+
+    public static void main(String[] args) {
+
+        // set: A collection of unique values (no duplicates allowed)
+        //      that can perform the following operations efficiently:
+        //      add, remove, search (contains), size, isEmpty, clear, equals
+
+        // We don't think of a set as having indexes; we just add things
+        // to the set in general and don't worry about order
+
+        // Set implementation:
+        // In Java, sets are represented by the Set interface in java.util
+        // Set is implemented by HashSet, TreeSet, and LinkedHashSet
+
+        //
+    }
 }
