0cc500ca1dc927e54c72195e12e89093710c4582
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2023-04-26 22:27:20 -0700

    Update

--- src/main/java/com/omar/learn/cs145_new/SetEx.java
+++ src/main/java/com/omar/learn/cs145_new/SetEx.java
@@ -36,5 +36,37 @@ public class SetEx {
         Set<Integer> set_a = new TreeSet<>();
         Set<Integer> set_b = new HashSet<>();
         Set<String> set_c = new HashSet<>(list);
+
+        // Sets and Ordering
+        // HashSet: elements are stored in unpredictable order
+        Set<String> names = new HashSet<>();
+        names.add("Omar");
+        names.add("Tim");
+        names.add("ff");
+        names.add("dd");
+        System.out.println(names); // [ff, dd, Tim, Omar]
+
+        // TreeSet: elements are stored in their "natural" sorted order
+        Set<String> names2 = new TreeSet<>();
+        names2.add("Omar");
+        names2.add("Tim");
+        names2.add("ff");
+        names2.add("dd");
+        names2.add("bb");
+        names2.add("aa");
+        System.out.println(names2); // [Omar, Tim, aa, bb, dd, ff]
+
+        // LinkedHashSet: elements are stored in the order they were inserted
+        Set<String> names3 = new LinkedHashSet<>();
+        names3.add("Omar");
+        names3.add("Tim");
+        names3.add("ff");
+        names3.add("dd");
+        System.out.println(names3); // [Omar, Tim, ff, dd]
+
+        // For each loop
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
