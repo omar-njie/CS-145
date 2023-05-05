3740b5b402bbdb806df36205e02755854ce0342d
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2023-05-05 06:45:03 -0700

    Update

--- src/main/java/com/omar/learn/cs145_new/SetsAndMaps.java
+++ src/main/java/com/omar/learn/cs145_new/SetsAndMaps.java
@@ -1,5 +1,7 @@
 package com.omar.learn.cs145_new;
 
+import java.util.*;
+
 /**
  * @author Omar
  * @version 5.3.23
@@ -7,6 +9,30 @@ package com.omar.learn.cs145_new;
 public class SetsAndMaps {
 
     public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(7);
+        list.add(4);
+        list.add(-9);
+        list.add(4);
+        list.add(15);
+        list.add(8);
+        list.add(27);
+        list.add(7);
+        list.add(11);
+        list.add(-5);
+        list.add(32);
+        list.add(-9);
+        list.add(-9);
+        sortAndRemoveDuplicates(list);
+
+    }
+
+    static void sortAndRemoveDuplicates(List<Integer> list) {
+        Set<Integer> rm = new HashSet<>(list);
+        List<Integer> sorted = new ArrayList<>(rm);
+        Collections.sort(sorted);
+        System.out.println(sorted);
 
     }
 }
