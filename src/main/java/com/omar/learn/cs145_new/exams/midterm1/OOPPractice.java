ae10f6a3aefe29c47bd70938c1f0c74b42ea49d7
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2023-05-09 23:19:41 -0700

    Update

---
+++ src/main/java/com/omar/learn/cs145_new/exams/midterm1/OOPPractice.java
@@ -0,0 +1,12 @@
+package com.omar.learn.cs145_new.exams.midterm1;
+
+/**
+ * @author omar
+ * @version 5/9/23
+ */
+public class OOPPractice {
+
+    public static void main(String[] args) {
+
+    }
+}
