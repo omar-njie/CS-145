a5b463760257e593aa6d6a647894c23c658805f3
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2023-05-10 23:28:20 -0700

    Update

--- src/main/java/com/omar/learn/cs145_new/exams/midterm1/SetsAndMapsPractice.java
+++ src/main/java/com/omar/learn/cs145_new/exams/midterm1/SetsAndMapsPractice.java
@@ -1,5 +1,10 @@
 package com.omar.learn.cs145_new.exams.midterm1;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author omar
  * @version 5/9/23
@@ -7,6 +12,34 @@ package com.omar.learn.cs145_new.exams.midterm1;
 public class SetsAndMapsPractice {
 
     public static void main(String[] args) {
+
+        List<Integer> lst = new ArrayList<>();
+        lst.add(1);
+        lst.add(1);
+        lst.add(2);
+
+
+        System.out.println(maxOccurences(lst));
+
+    }
+
+    static int maxOccurences(List<Integer> lst) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (Integer i : lst) {
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 0);
+            }
+        }
+        int max = 0;
+        for (Integer i : map.keySet()) {
+            if (map.get(i) > max) {
+                max = map.get(i);
+            }
+        }
+        return max;
+
 
     }
 }
--- src/main/java/com/omar/learn/cs145_new/exams/midterm1/oop/OOPPractice.java
+++ src/main/java/com/omar/learn/cs145_new/exams/midterm1/oop/OOPPractice.java
@@ -8,5 +8,89 @@ public class OOPPractice {
 
     public static void main(String[] args) {
 
+        Foo foo;
+        Bar bar = new Bar();
+        Baz baz = new Baz();
+        Moo moo = new Moo();
+
+        //(Foo) moo;
+        //(Moo) baz;
+        Foo[] a = new Bar[10];
+        // (Moo) bar;
+        //(Bar) baz;
+
+        Alpha var1 = new Gamma();
+        Beta var2 = new Delta();
+        Alpha var3 = new Beta();
+        Object var4 = new Beta();
+
+        //not//var4.method1(); //will print Alpha 1 since var4 is a Beta reference.
+
+        ((Beta) var4).method1(); //will print Alpha 1.
+
+        //((Delta) var1).method1(); //will invoke method1() from the Delta class.
+
+        //((Delta) var1).method1(); //will cause a casting error.
+
+        ///var4.method1(); //will cause a compilation error.
+
+        ((Beta) var4).method1(); //will cause a casting error.
+
+
+        ((Delta) var2).method3(); //will cause a casting error.
+
+        //((Delta) var2).method3(); //will print Delta 2.
+
+        //((Gamma) var2).method3(); //will cause a casting error.
+
+        //((Gamma) var2).method3(); //will invoke method3() from the Gamma class.
+
     }
+}
+
+class Foo {}
+
+class Bar extends Foo {}
+
+class Baz extends Foo {}
+
+class Moo extends Baz {}
+
+class Alpha {
+    public void method1() {
+        System.out.println("Alpha 1");
+    }
+
+    public void method2() {
+        System.out.println("Alpha 2");
+    }
+}
+
+class Gamma extends Beta {
+    public void method1() {
+        System.out.println("Gamma 1");
+        super.method2();
+    }
+
+    public void method3() {
+        System.out.println("Gamma 3");
+    }
+}
+
+class Beta extends Alpha {
+    public void method2() {
+        System.out.println("Beta 2");
+        super.method2();
+    }
+}
+
+class Delta extends Beta {
+   public void method2() {
+       System.out.println("Delta 2");
+    }
+
+   public void method3() {
+       method2();
+       System.out.println("Delta 3");
+   }
 }
